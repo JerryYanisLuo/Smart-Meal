c98007e3aeddca51384fda0d24770bd37298e951
Yifan Luo <yf.luo@trip.com>
2021-08-27 21:07:56 +0800

    init project

---
+++ src/main/java/com/example/smartmeal/SmartMealApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartmeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartMealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smartmeal/SmartMealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartmeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
